2d0e05d9bf61329931b511b2f2b781b8825b5253
Joaquin Cabezas <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2025-01-14 14:00:26 +0100

    Actualizacion

---
+++ src/components/PlaylistsManager.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>PlaylistManager</h1>
+    <p v-for="song in songs" :key="song.id">
+        <PlaylistItem :song="song"></PlaylistItem>
+    </p>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+li:hover{
+    background-color: none;
+}
+</style>
